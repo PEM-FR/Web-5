555b32b793871da6bee2385ca99c2c38c1fba066
PEM-FR <pem1337@free.fr>
2011-10-26 00:35:41 +0200

    Added filtering capabilities to speaker's page, also corrected english typos

---
+++ scripts/sorter.js
@@ -0,0 +1,43 @@
+dojo.ready(function(){
+    
+    var tags = {
+        html5 : [],
+        webgl : [],
+        dojo  : [],
+        javascript : [],
+        perfs  : [],
+        mobile  : []
+        },
+        filteredNodes = []
+    ;
+    
+    
+    
+    
+    dojo.query(".tagHeader").forEach(function(tagHeader){
+        var idTagHeader = dojo.attr(tagHeader, "id");
+        if(idTagHeader != "all"){
+            tags[idTagHeader] = dojo.query("div[data-tag-list~=\"" + idTagHeader + "\"]");
+            tagHeader.innerHTML += " (" + tags[idTagHeader].length + ")";
+        }
+        dojo.connect(tagHeader, "onclick", dojo.hitch(this, function(){
+            filteredNodes = [];
+            if(idTagHeader == "all"){
+                for(tagType in tags){
+                    tags[tagType].forEach(function(node){
+                        dojo.style(node, "display", "block");
+                    }, this);
+                }
+            }else{
+                for(tagType in tags){
+                    tags[tagType].forEach(function(node){
+                        if(tagType == idTagHeader){
+                            filteredNodes.push(node);
+                        }
+                        dojo.style(node, "display", ((tagType != idTagHeader && filteredNodes.indexOf(node) == -1) ? "none" : "block"));
+                    }, this);
+                }
+            }
+        }));
+    }, this);
+});
